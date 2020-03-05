180b13d86f62857cd9bc28cdc00498cdf2693dc5
jmalue <jrod2416@gmail.com>
2020-03-05 17:59:45 -0500

    Added Modal

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// toggle button
 function myFunction() {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
@@ -7,13 +34,16 @@ function myFunction() {
       x.style.display = "none";
     }
 
-var slideIndex = 1;
+    // slides
+    var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -22,13 +52,10 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  console.log(dots);
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   else if (n < 1) {slideIndex = slides.length}
-  else {slideIndex = n;}
-  
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  else if (i = 0); i < slides.length; i++; {
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
